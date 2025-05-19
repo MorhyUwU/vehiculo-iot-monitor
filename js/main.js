3bfb31d3c89169bb816a4e38755d00534f7d4866
Hector efrain De la rosa Enciso <144378700+MorhyUwU@users.noreply.github.com>
2025-05-19 11:27:17 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const API_URL = "http://34.205.55.185:5000/api/devices";
+const API_URL = "http://34.232.50.27:5000/api/devices";
 const tableBody = document.getElementById("deviceTableBody");
 const currentStatus = document.getElementById("currentStatus");
 
